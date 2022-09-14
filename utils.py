7e4990b63aca6d9aa6364df621021bef0781bdbb
lucas-baier <lucas.baier@kit.edu>
2022-09-14 21:13:32 +0200

    Remove deprecated package

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from deprecated.sequence import EventSeq, ControlSeq
+# from deprecated.sequence import EventSeq, ControlSeq
 import tensorflow as tf
 import params as par
 
@@ -20,47 +20,47 @@ def find_files_by_extensions(root, exts=[]):
                 yield os.path.join(path, name)
 
 
-def event_indeces_to_midi_file(event_indeces, midi_file_name, velocity_scale=0.8):
-    event_seq = EventSeq.from_array(event_indeces)
-    note_seq = event_seq.to_note_seq()
-    for note in note_seq.notes:
-        note.velocity = int((note.velocity - 64) * velocity_scale + 64)
-    note_seq.to_midi_file(midi_file_name)
-    return len(note_seq.notes)
+# def event_indeces_to_midi_file(event_indeces, midi_file_name, velocity_scale=0.8):
+#     event_seq = EventSeq.from_array(event_indeces)
+#     note_seq = event_seq.to_note_seq()
+#     for note in note_seq.notes:
+#         note.velocity = int((note.velocity - 64) * velocity_scale + 64)
+#     note_seq.to_midi_file(midi_file_name)
+#     return len(note_seq.notes)
 
 
-def transposition(events, controls, offset=0):
-    # events [steps, batch_size, event_dim]
-    # return events, controls
+# def transposition(events, controls, offset=0):
+#     # events [steps, batch_size, event_dim]
+#     # return events, controls
 
-    events = np.array(events, dtype=np.int64)
-    controls = np.array(controls, dtype=np.float32)
-    event_feat_ranges = EventSeq.feat_ranges()
+#     events = np.array(events, dtype=np.int64)
+#     controls = np.array(controls, dtype=np.float32)
+#     event_feat_ranges = EventSeq.feat_ranges()
 
-    on = event_feat_ranges['note_on']
-    off = event_feat_ranges['note_off']
+#     on = event_feat_ranges['note_on']
+#     off = event_feat_ranges['note_off']
 
-    if offset > 0:
-        indeces0 = (((on.start <= events) & (events < on.stop - offset)) |
-                    ((off.start <= events) & (events < off.stop - offset)))
-        indeces1 = (((on.stop - offset  <= events) & (events < on.stop)) |
-                    ((off.stop - offset <= events) & (events < off.stop)))
-        events[indeces0] += offset
-        events[indeces1] += offset - 12
-    elif offset < 0:
-        indeces0 = (((on.start - offset <= events) & (events < on.stop)) |
-                    ((off.start - offset <= events) & (events < off.stop)))
-        indeces1 = (((on.start <= events) & (events < on.start - offset)) |
-                    ((off.start <= events) & (events < off.start - offset)))
-        events[indeces0] += offset
-        events[indeces1] += offset + 12
+#     if offset > 0:
+#         indeces0 = (((on.start <= events) & (events < on.stop - offset)) |
+#                     ((off.start <= events) & (events < off.stop - offset)))
+#         indeces1 = (((on.stop - offset  <= events) & (events < on.stop)) |
+#                     ((off.stop - offset <= events) & (events < off.stop)))
+#         events[indeces0] += offset
+#         events[indeces1] += offset - 12
+#     elif offset < 0:
+#         indeces0 = (((on.start - offset <= events) & (events < on.stop)) |
+#                     ((off.start - offset <= events) & (events < off.stop)))
+#         indeces1 = (((on.start <= events) & (events < on.start - offset)) |
+#                     ((off.start <= events) & (events < off.start - offset)))
+#         events[indeces0] += offset
+#         events[indeces1] += offset + 12
 
-    assert ((0 <= events) & (events < EventSeq.dim())).all()
-    histr = ControlSeq.feat_ranges()['pitch_histogram']
-    controls[:, :, histr.start:histr.stop] = np.roll(
-                    controls[:, :, histr.start:histr.stop], offset, -1)
+#     assert ((0 <= events) & (events < EventSeq.dim())).all()
+#     histr = ControlSeq.feat_ranges()['pitch_histogram']
+#     controls[:, :, histr.start:histr.stop] = np.roll(
+#                     controls[:, :, histr.start:histr.stop], offset, -1)
 
-    return events, controls
+#     return events, controls
 
 
 def dict2params(d, f=','):
